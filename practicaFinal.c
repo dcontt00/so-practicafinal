e0891e63fa05115709f217f65c562313236c557e
dcontt00 <dcontt00@estudiantes.unileon.es>
2020-12-22 12:46:08 +0100

    Añadido writeLogMessage

---
+++ practicaFinal.c
@@ -0,0 +1,15 @@
+#include <time.h>
+
+char logFileName;
+
+void writeLogMessage(char *id, char *msg) {
+    // Calculamos la hora actual
+    time_t now = time(0);
+    struct tm *tlocal = localtime(&now);
+    char stnow[25];
+    strftime(stnow, 25, "%d/%m/%y %H:%M:%S", tlocal);
+    // Escribimos en el log
+    logFile = fopen(logFileName, "a");
+    fprintf(logFile, "[%s] %s: %s\n", stnow, id, msg);
+    fclose(logFile);
+}
